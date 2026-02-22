74c34ff456cc50ef970fb42905adc5a685d411ff
Ricardo Rodriguez <lucario.the.hero@gmail.com>
2026-02-22 00:49:59 +0000

    Edit For Testing

--- Main.java
+++ Main.java
@@ -79,9 +79,9 @@ int age = difference/365;
 
 	/***** OUTPUT SECTION *****/
 System.out.println("Julian Day Number for today's date, " + todayDate + ", is " + JDNOne);
-
+System.out.println("");
 System.out.println("Julian Day Number for birthday, " + BDay + ", is " + JDNTwo);
-
+System.out.println("");
 System.out.println("The difference in days is " + difference + ", which makes you approximately " + age +
 " years old!");
   }																			
